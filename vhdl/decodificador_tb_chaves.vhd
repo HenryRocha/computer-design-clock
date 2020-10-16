bb02322dc0ee80be8944b7c1f978952de2a73faa
Henry Rocha <henryrocha@protonmail.com>
2020-10-16 14:47:27 -0300

    change test names and entity name

--- vhdl/decodificador_tb_chaves.vhd
+++ vhdl/decodificador_tb_chaves.vhd
@@ -5,13 +5,13 @@ USE ieee.numeric_std.ALL;
 LIBRARY vunit_lib;
 CONTEXT vunit_lib.vunit_context;
 
-ENTITY decodificador_tb IS
+ENTITY decodificador_tb_chaves IS
     GENERIC (
         runner_cfg : STRING := runner_cfg_default
     );
 END ENTITY;
 
-ARCHITECTURE tb OF decodificador_tb IS
+ARCHITECTURE tb OF decodificador_tb_chaves IS
     -- Inputs and ouputs
     SIGNAL enderecoRAMROM           : std_logic_vector(11 DOWNTO 0);
     SIGNAL controleDecodificador    : std_logic_vector(5 DOWNTO 0);
@@ -43,52 +43,52 @@ BEGIN
         test_runner_setup(runner, runner_cfg);
 
         WHILE test_suite LOOP
-            IF run("test_chaves_0") THEN
+            IF run("habLeitura_sw0") THEN
                 enderecoRAMROM           <= "000000000000";
                 expControleDecodificador <= "010000";
                 WAIT UNTIL clk = '1';
                 check_equal(controleDecodificador, expControleDecodificador);
-            ELSIF run("test_chaves_1") THEN
+            ELSIF run("habLeitura_sw1") THEN
                 enderecoRAMROM           <= "000000000001";
                 expControleDecodificador <= "010000";
                 WAIT UNTIL clk = '1';
                 check_equal(controleDecodificador, expControleDecodificador);
-            ELSIF run("test_chaves_2") THEN
+            ELSIF run("habLeitura_sw2") THEN
                 enderecoRAMROM           <= "000000000010";
                 expControleDecodificador <= "010000";
                 WAIT UNTIL clk = '1';
                 check_equal(controleDecodificador, expControleDecodificador);
-            ELSIF run("test_chaves_3") THEN
+            ELSIF run("habLeitura_sw3") THEN
                 enderecoRAMROM           <= "000000000011";
                 expControleDecodificador <= "010000";
                 WAIT UNTIL clk = '1';
                 check_equal(controleDecodificador, expControleDecodificador);
-            ELSIF run("test_chaves_4") THEN
+            ELSIF run("habLeitura_sw4") THEN
                 enderecoRAMROM           <= "000000000100";
                 expControleDecodificador <= "010000";
                 WAIT UNTIL clk = '1';
                 check_equal(controleDecodificador, expControleDecodificador);
-            ELSIF run("test_chaves_5") THEN
+            ELSIF run("habLeitura_sw5") THEN
                 enderecoRAMROM           <= "000000000101";
                 expControleDecodificador <= "010000";
                 WAIT UNTIL clk = '1';
                 check_equal(controleDecodificador, expControleDecodificador);
-            ELSIF run("test_chaves_6") THEN
+            ELSIF run("habLeitura_sw6") THEN
                 enderecoRAMROM           <= "000000000110";
                 expControleDecodificador <= "010000";
                 WAIT UNTIL clk = '1';
                 check_equal(controleDecodificador, expControleDecodificador);
-            ELSIF run("test_chaves_7") THEN
+            ELSIF run("habLeitura_sw7") THEN
                 enderecoRAMROM           <= "000000000111";
                 expControleDecodificador <= "010000";
                 WAIT UNTIL clk = '1';
                 check_equal(controleDecodificador, expControleDecodificador);
-            ELSIF run("test_chaves_8") THEN
+            ELSIF run("habLeitura_sw8") THEN
                 enderecoRAMROM           <= "000000001000";
                 expControleDecodificador <= "010000";
                 WAIT UNTIL clk = '1';
                 check_equal(controleDecodificador, expControleDecodificador);
-            ELSIF run("test_chaves_9") THEN
+            ELSIF run("habLeitura_sw9") THEN
                 enderecoRAMROM           <= "000000001001";
                 expControleDecodificador <= "010000";
                 WAIT UNTIL clk = '1';
