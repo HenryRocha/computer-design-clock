3830286ea9f04c963c75777a7ab6f055803a5408
Henry Rocha <henryfr1@al.insper.edu.br>
2020-10-17 14:22:07 -0300

    Add comments and generics to ULA

--- vhdl/ULA.vhd
+++ vhdl/ULA.vhd
@@ -4,30 +4,38 @@ USE ieee.numeric_std.ALL; -- Biblioteca IEEE para funÃ§Ãµes aritmÃ©ticas
 
 ENTITY ULA IS
     GENERIC (
-        larguraDados : NATURAL := 8
+        larguraDados : NATURAL := 8;
+        SEL_WIDTH    : NATURAL := 3
     );
     PORT (
-        entradaA, entradaB : IN STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
-        seletor            : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-        saida              : OUT STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
-        flagZero           : OUT std_logic
+        -- Input ports 
+        entradaA : IN STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
+        entradaB : IN STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
+        seletor  : IN STD_LOGIC_VECTOR(SEL_WIDTH - 1 DOWNTO 0);
+
+        -- Output ports
+        saida    : OUT STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
+        flagZero : OUT std_logic
     );
 END ENTITY;
 
 ARCHITECTURE comportamento OF ULA IS
+    -- Declarando a constante "zero", usada para determinar se a saída é zero ou não.
     CONSTANT zero : std_logic_vector(larguraDados - 1 DOWNTO 0) := (OTHERS => '0');
 
+    -- Todas as operações que a ULA consegue realizar.
     SIGNAL op_soma_zero   : STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
     SIGNAL op_soma        : STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
     SIGNAL op_soma_b_zero : STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
     SIGNAL op_soma_a_zero : STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
+    SIGNAL op_sub         : STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
+    SIGNAL op_or          : STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
+    SIGNAL op_and         : STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
 
-    SIGNAL op_sub : STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
-    SIGNAL op_or  : STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
-    SIGNAL op_and : STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
-
-    SIGNAL temp : STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
+    -- Sinal temporário usado durante a comparação com a constante "zero".
+    SIGNAL saidaTemp : STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
 BEGIN
+    -- Realizando todas as operações.
     op_soma_zero   <= zero;
     op_soma        <= STD_LOGIC_VECTOR(unsigned(entradaA) + unsigned(entradaB));
     op_soma_b_zero <= STD_LOGIC_VECTOR(unsigned(entradaB));
@@ -36,7 +44,8 @@ BEGIN
     op_and         <= entradaA AND entradaB;
     op_or          <= entradaA OR entradaB;
 
-    temp <= op_soma_zero WHEN (seletor = "000") ELSE
+    -- Verificando qual operação deve ser atribuída a "saidaTemp".
+    saidaTemp <= op_soma_zero WHEN (seletor = "000") ELSE
         op_soma WHEN (seletor = "001") ELSE
         op_sub WHEN (seletor = "010") ELSE
         op_soma_b_zero WHEN (seletor = "011") ELSE
@@ -45,7 +54,9 @@ BEGIN
         op_and WHEN (seletor = "110") ELSE
         entradaA;
 
-    saida    <= temp;
-    flagZero <= '1' WHEN unsigned(temp) = unsigned(zero) ELSE
+    -- Atribuindo "saida" a "saidaTemp" e realizando a comparação para verificar se
+    -- o resultado é zero.
+    saida    <= saidaTemp;
+    flagZero <= '1' WHEN unsigned(saidaTemp) = unsigned(zero) ELSE
         '0';
 END ARCHITECTURE;
