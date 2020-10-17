8f388730ea0c960ae87e70661c59016c5483e82d
Henry Rocha <henryfr1@al.insper.edu.br>
2020-10-17 13:52:29 -0300

    Add comments to unidadeControle

--- vhdl/unidadeControle.vhd
+++ vhdl/unidadeControle.vhd
@@ -15,6 +15,7 @@ ENTITY unidadeControle IS
 END ENTITY;
 
 ARCHITECTURE main OF unidadeControle IS
+    -- Declarando onde cada ponto de controle será localizado na palavra de controle.
     ALIAS muxProxPC     : std_logic IS palavraControle(7);
     ALIAS muxImedDados  : std_logic IS palavraControle(6);
     ALIAS habEscritaReg : std_logic IS palavraControle(5);
@@ -22,21 +23,11 @@ ARCHITECTURE main OF unidadeControle IS
     ALIAS habLeituraRAM : std_logic IS palavraControle(1);
     ALIAS habEscritaRAM : std_logic IS palavraControle(0);
 
-    SIGNAL instrucao    : std_logic_vector(12 DOWNTO 0);
-    CONSTANT opcodeAdd  : std_logic_vector(3 DOWNTO 0) := "0000";
-    CONSTANT opcodeAddi : std_logic_vector(3 DOWNTO 0) := "0001";
-    CONSTANT opcodeSub  : std_logic_vector(3 DOWNTO 0) := "0010";
-    CONSTANT opcodeSubi : std_logic_vector(3 DOWNTO 0) := "0011";
-    CONSTANT opcodeMovm : std_logic_vector(3 DOWNTO 0) := "0100";
-    CONSTANT opcodeMovr : std_logic_vector(3 DOWNTO 0) := "0101";
-    CONSTANT opcodeLea  : std_logic_vector(3 DOWNTO 0) := "0110";
-    CONSTANT opcodeCmp  : std_logic_vector(3 DOWNTO 0) := "0111";
-    CONSTANT opcodeJE   : std_logic_vector(3 DOWNTO 0) := "1000";
-    CONSTANT opcodeJNE  : std_logic_vector(3 DOWNTO 0) := "1001";
-    CONSTANT opcodeJMP  : std_logic_vector(3 DOWNTO 0) := "1010";
-    CONSTANT opcodeOr   : std_logic_vector(3 DOWNTO 0) := "1011";
-    CONSTANT opcodeAnd  : std_logic_vector(3 DOWNTO 0) := "1100";
-
+    -- O sinal "instrucao" é responsável por dizer qual instrução está sendo executada.
+    -- Desse modo, ele é um vetor onde o tamanho é o número de instruções que o
+    -- processador tem.
+    SIGNAL instrucao : std_logic_vector(12 DOWNTO 0);
+    -- Declarando qual bit do vetor é cada instrução.
     ALIAS add     : std_logic IS instrucao(0);
     ALIAS addi    : std_logic IS instrucao(1);
     ALIAS sub     : std_logic IS instrucao(2);
@@ -50,7 +41,23 @@ ARCHITECTURE main OF unidadeControle IS
     ALIAS jmp     : std_logic IS instrucao(10);
     ALIAS instOR  : std_logic IS instrucao(11);
     ALIAS instAND : std_logic IS instrucao(12);
+
+    -- Declarando todas as intruções da CPU e seus OpCodes.
+    CONSTANT opcodeAdd  : std_logic_vector(3 DOWNTO 0) := "0000";
+    CONSTANT opcodeAddi : std_logic_vector(3 DOWNTO 0) := "0001";
+    CONSTANT opcodeSub  : std_logic_vector(3 DOWNTO 0) := "0010";
+    CONSTANT opcodeSubi : std_logic_vector(3 DOWNTO 0) := "0011";
+    CONSTANT opcodeMovm : std_logic_vector(3 DOWNTO 0) := "0100";
+    CONSTANT opcodeMovr : std_logic_vector(3 DOWNTO 0) := "0101";
+    CONSTANT opcodeLea  : std_logic_vector(3 DOWNTO 0) := "0110";
+    CONSTANT opcodeCmp  : std_logic_vector(3 DOWNTO 0) := "0111";
+    CONSTANT opcodeJE   : std_logic_vector(3 DOWNTO 0) := "1000";
+    CONSTANT opcodeJNE  : std_logic_vector(3 DOWNTO 0) := "1001";
+    CONSTANT opcodeJMP  : std_logic_vector(3 DOWNTO 0) := "1010";
+    CONSTANT opcodeOr   : std_logic_vector(3 DOWNTO 0) := "1011";
+    CONSTANT opcodeAnd  : std_logic_vector(3 DOWNTO 0) := "1100";
 BEGIN
+    -- Verificando qual a instrução a ser executada.
     WITH opcode SELECT
         instrucao <= "0000000000001" WHEN opcodeAdd,
         "0000000000010" WHEN opcodeAddi,
@@ -67,6 +74,8 @@ BEGIN
         "1000000000000" WHEN opcodeAnd,
         "0000000000000" WHEN OTHERS;
 
+    -- Toda a lógica a seguir foi feita a partir da tabela INSTRUÇÃO x PONTO DE CONTROLE.
+    -- Um link para essa tabela está disponível no README.md do repositório do projeto.
     muxProxPC <= jmp OR (je AND flagZero) OR (jne AND (NOT flagZero));
 
     muxImedDados <= add OR sub OR movm OR instOR OR instAND;
