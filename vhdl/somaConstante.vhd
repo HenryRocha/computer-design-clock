1647bad0f9dbcf6d4b5bcfd203c1599a8ddf542d
Henry Rocha <henryfr1@al.insper.edu.br>
2020-10-17 14:33:50 -0300

    Add comments to somaConstante

--- vhdl/somaConstante.vhd
+++ vhdl/somaConstante.vhd
@@ -1,6 +1,6 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-USE ieee.numeric_std.ALL; --Soma (esta biblioteca =ieee)
+USE ieee.numeric_std.ALL;
 
 ENTITY somaConstante IS
     GENERIC (
@@ -8,12 +8,16 @@ ENTITY somaConstante IS
         constante    : NATURAL := 4
     );
     PORT (
+        -- Input ports
         entrada : IN STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
-        saida   : OUT STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0)
+
+        -- Output ports
+        saida : OUT STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0)
     );
 END ENTITY;
 
 ARCHITECTURE comportamento OF somaConstante IS
 BEGIN
+    -- A saída é a soma da entrada com a constante.
     saida <= std_logic_vector(unsigned(entrada) + constante);
 END ARCHITECTURE;
