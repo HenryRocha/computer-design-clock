c5c964e2fd18fe0468356d3d74d0f59d0882486a
Henry Rocha <henryfr1@al.insper.edu.br>
2020-10-17 14:22:20 -0300

    Add more tests for ULA

--- vhdl/ULA_tb.vhd
+++ vhdl/ULA_tb.vhd
@@ -6,16 +6,25 @@ CONTEXT vunit_lib.vunit_context;
 
 ENTITY ULA_tb IS
     GENERIC (
-        runner_cfg : STRING := runner_cfg_default
+        runner_cfg : STRING  := runner_cfg_default;
+        DATA_WIDTH : NATURAL := 8;
+        SEL_WIDTH  : NATURAL := 3
     );
 END ENTITY;
 
 ARCHITECTURE tb OF ULA_tb IS
-    CONSTANT DATA_WIDTH : NATURAL := 8;
-    -- Inputs and ouputs
-    SIGNAL muxImedDados_out, bancoReg_out, ULA_out, expectedResult : std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
-    SIGNAL operacao                                                : std_logic_vector(2 DOWNTO 0);
-    SIGNAL flagZero                                                : std_logic;
+    -- Input ports 
+    SIGNAL entradaA : std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
+    SIGNAL entradaB : std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
+    SIGNAL operacao : std_logic_vector(SEL_WIDTH - 1 DOWNTO 0);
+
+    -- Output ports
+    SIGNAL saida    : std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
+    SIGNAL flagZero : std_logic;
+
+    -- Sinais usado para verificar as saída.
+    SIGNAL expectedSaida    : std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
+    SIGNAL expectedFlagZero : std_logic;
 
     -- Clock
     CONSTANT CLK_PERIOD : TIME      := 1 ns;
@@ -34,10 +43,10 @@ BEGIN
             larguraDados => DATA_WIDTH
         )
         PORT MAP(
-            entradaA => muxImedDados_out,
-            entradaB => bancoReg_out,
+            entradaA => entradaA,
+            entradaB => entradaB,
             seletor  => operacao,
-            saida    => ULA_out,
+            saida    => saida,
             flagZero => flagZero
         );
 
@@ -46,32 +55,68 @@ BEGIN
         test_runner_setup(runner, runner_cfg);
 
         WHILE test_suite LOOP
-            IF run("test_ADD_A_0") THEN
-                muxImedDados_out <= "00000111";
-                bancoReg_out     <= "00000001";
-                operacao         <= "100";
-                expectedResult   <= "00000111";
+            IF run("soma_zero") THEN
+                entradaA      <= "10000001";
+                entradaB      <= "10000001";
+                operacao      <= "000";
+                expectedSaida <= "00000000";
+
                 WAIT UNTIL clk = '1';
-                ASSERT(ULA_out = expectedResult)
-                REPORT (
-                    "ULA_out mismatch. Expected: "
-                    & to_string(expectedResult)
-                    & ". Got: "
-                    & to_string(ULA_out))
-                    SEVERITY error;
-            ELSIF run("test_ADD_B_0") THEN
-                muxImedDados_out <= "00000001";
-                bancoReg_out     <= "00000111";
-                operacao         <= "011";
-                expectedResult   <= "00000111";
+                check_equal(saida, expectedSaida);
+
+            ELSIF run("soma") THEN
+                entradaA      <= "00000101";
+                entradaB      <= "00000101";
+                operacao      <= "001";
+                expectedSaida <= "00001010";
+
                 WAIT UNTIL clk = '1';
-                ASSERT(ULA_out = expectedResult)
-                REPORT (
-                    "ULA_out mismatch. Expected: "
-                    & to_string(expectedResult)
-                    & ". Got: "
-                    & to_string(ULA_out))
-                    SEVERITY error;
+                check_equal(saida, expectedSaida);
+
+            ELSIF run("soma_b_zero") THEN
+                entradaA      <= "10000100";
+                entradaB      <= "10000001";
+                operacao      <= "011";
+                expectedSaida <= "10000001";
+
+                WAIT UNTIL clk = '1';
+                check_equal(saida, expectedSaida);
+
+            ELSIF run("soma_a_zero") THEN
+                entradaA      <= "10000001";
+                entradaB      <= "10000100";
+                operacao      <= "100";
+                expectedSaida <= "10000001";
+
+                WAIT UNTIL clk = '1';
+                check_equal(saida, expectedSaida);
+
+            ELSIF run("sub") THEN
+                entradaA      <= "10000011";
+                entradaB      <= "10000000";
+                operacao      <= "010";
+                expectedSaida <= "00000011";
+
+                WAIT UNTIL clk = '1';
+                check_equal(saida, expectedSaida);
+
+            ELSIF run("and") THEN
+                entradaA      <= "10000011";
+                entradaB      <= "10000011";
+                operacao      <= "110";
+                expectedSaida <= "10000011";
+
+                WAIT UNTIL clk = '1';
+                check_equal(saida, expectedSaida);
+
+            ELSIF run("or") THEN
+                entradaA      <= "10000001";
+                entradaB      <= "00000010";
+                operacao      <= "101";
+                expectedSaida <= "10000011";
+
+                WAIT UNTIL clk = '1';
+                check_equal(saida, expectedSaida);
             END IF;
         END LOOP;
 
