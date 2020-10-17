b20befb219c149738dbb2fa8e2ba22ca9495e11e
Henry Rocha <henryfr1@al.insper.edu.br>
2020-10-17 19:54:34 -0300

    Add registers to interfaceHEX

--- vhdl/interfaceHEX.vhd
+++ vhdl/interfaceHEX.vhd
@@ -9,6 +9,7 @@ ENTITY interfaceHEX IS
     );
     PORT (
         -- Input ports
+        clk      : IN std_logic;
         endereco : IN std_logic_vector(ADDR_WIDTH - 1 DOWNTO 0);
         habilita : IN std_logic := '0';
         dados    : IN std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
@@ -26,6 +27,14 @@ ARCHITECTURE main OF interfaceHEX IS
     SIGNAL interHEX3 : std_logic_vector(HEX_WIDTH - 1 DOWNTO 0) := "1111111";
     SIGNAL interHEX4 : std_logic_vector(HEX_WIDTH - 1 DOWNTO 0) := "1111111";
     SIGNAL interHEX5 : std_logic_vector(HEX_WIDTH - 1 DOWNTO 0) := "1111111";
+
+    -- Declarando o habilita de cada um dos HEX.
+    SIGNAL habHEX0 : std_logic;
+    SIGNAL habHEX1 : std_logic;
+    SIGNAL habHEX2 : std_logic;
+    SIGNAL habHEX3 : std_logic;
+    SIGNAL habHEX4 : std_logic;
+    SIGNAL habHEX5 : std_logic;
 BEGIN
     -- Declarando todos os conversores de 7 segmentos.
     conversorHex0 : ENTITY work.conversorHex7Seg
@@ -82,16 +91,90 @@ BEGIN
         );
 
     -- Cada display só será escrito caso o endereço seja o referente aquela
-    -- dupla de display. 
-    HEX0 <= interHEX0 WHEN (endereco = "100000000000" AND habilita = '1');
+    -- dupla de display.
+    habHEX0 <= '1' WHEN (endereco = "100000000000" AND habilita = '1') ELSE
+        '0';
+    habHEX1 <= '1' WHEN (endereco = "100000000000" AND habilita = '1') ELSE
+        '0';
+    habHEX2 <= '1' WHEN (endereco = "100000000001" AND habilita = '1') ELSE
+        '0';
+    habHEX3 <= '1' WHEN (endereco = "100000000001" AND habilita = '1') ELSE
+        '0';
+    habHEX4 <= '1' WHEN (endereco = "100000000010" AND habilita = '1') ELSE
+        '0';
+    habHEX5 <= '1' WHEN (endereco = "100000000010" AND habilita = '1') ELSE
+        '0';
 
-    HEX1 <= interHEX1 WHEN (endereco = "100000000000" AND habilita = '1');
+    -- Declarando os registradores de cada HEX, para guardar o valor de cada um.
+    register_hex0 : ENTITY work.registradorGenerico
+        GENERIC MAP(
+            larguraDados => HEX_WIDTH
+        )
+        PORT MAP(
+            DIN    => interHEX0,
+            DOUT   => HEX0,
+            ENABLE => habHEX0,
+            CLK    => clk,
+            RST    => '0'
+        );
 
-    HEX2 <= interHEX2 WHEN (endereco = "100000000001" AND habilita = '1');
+    register_hex1 : ENTITY work.registradorGenerico
+        GENERIC MAP(
+            larguraDados => HEX_WIDTH
+        )
+        PORT MAP(
+            DIN    => interHEX1,
+            DOUT   => HEX1,
+            ENABLE => habHEX1,
+            CLK    => clk,
+            RST    => '0'
+        );
 
-    HEX3 <= interHEX3 WHEN (endereco = "100000000001" AND habilita = '1');
+    register_hex2 : ENTITY work.registradorGenerico
+        GENERIC MAP(
+            larguraDados => HEX_WIDTH
+        )
+        PORT MAP(
+            DIN    => interHEX2,
+            DOUT   => HEX2,
+            ENABLE => habHEX2,
+            CLK    => clk,
+            RST    => '0'
+        );
 
-    HEX4 <= interHEX4 WHEN (endereco = "100000000010" AND habilita = '1');
+    register_hex3 : ENTITY work.registradorGenerico
+        GENERIC MAP(
+            larguraDados => HEX_WIDTH
+        )
+        PORT MAP(
+            DIN    => interHEX3,
+            DOUT   => HEX3,
+            ENABLE => habHEX3,
+            CLK    => clk,
+            RST    => '0'
+        );
 
-    HEX5 <= interHEX5 WHEN (endereco = "100000000010" AND habilita = '1');
+    register_hex4 : ENTITY work.registradorGenerico
+        GENERIC MAP(
+            larguraDados => HEX_WIDTH
+        )
+        PORT MAP(
+            DIN    => interHEX4,
+            DOUT   => HEX4,
+            ENABLE => habHEX4,
+            CLK    => clk,
+            RST    => '0'
+        );
+
+    register_hex5 : ENTITY work.registradorGenerico
+        GENERIC MAP(
+            larguraDados => HEX_WIDTH
+        )
+        PORT MAP(
+            DIN    => interHEX5,
+            DOUT   => HEX5,
+            ENABLE => habHEX5,
+            CLK    => clk,
+            RST    => '0'
+        );
 END ARCHITECTURE;
