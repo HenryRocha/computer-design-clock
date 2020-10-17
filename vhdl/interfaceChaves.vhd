1ce306baacac2890431a7d9ab5346af3b62cb09d
Henry Rocha <henryfr1@al.insper.edu.br>
2020-10-17 15:22:48 -0300

    Add comments and refactor interfaceChaves

--- vhdl/interfaceChaves.vhd
+++ vhdl/interfaceChaves.vhd
@@ -7,29 +7,31 @@ ENTITY interfaceChaves IS
         ADDR_WIDTH : NATURAL := 12
     );
     PORT (
+        -- Input ports
         entrada  : IN std_logic_vector(9 DOWNTO 0);
         endereco : IN std_logic_vector(ADDR_WIDTH - 1 DOWNTO 0);
         habilita : IN std_logic;
-        saida    : OUT std_logic_vector(DATA_WIDTH - 1 DOWNTO 0)
+
+        -- Output ports
+        saida : OUT std_logic_vector(DATA_WIDTH - 1 DOWNTO 0)
     );
 END ENTITY;
 
 ARCHITECTURE main OF interfaceChaves IS
-    SIGNAL temp : std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
 BEGIN
-    WITH endereco SELECT
-        temp(0) <= entrada(0) WHEN "000000000000",
-        entrada(1) WHEN "000000000001",
-        entrada(2) WHEN "000000000010",
-        entrada(3) WHEN "000000000011",
-        entrada(4) WHEN "000000000100",
-        entrada(5) WHEN "000000000101",
-        entrada(6) WHEN "000000000110",
-        entrada(7) WHEN "000000000111",
-        entrada(8) WHEN "000000001000",
-        entrada(9) WHEN "000000001010",
-        '0' WHEN OTHERS;
-
-    saida <= temp WHEN (habilita = '1') ELSE
-        (OTHERS => 'Z');
+    -- Apenas o bit 0 da saída será usado. Foi feito desse modo para não ser necessário
+    -- o uso do estendeSinalGenerico.
+    -- O sinal de saída será o valor lido de cada chave, mas o valor de saída só será
+    -- substituido caso o endereço seja o correto e a interface esteja habilitada.
+    saida(0) <= entrada(0) WHEN (endereco = "000000000000" AND habilita = '1') ELSE
+    entrada(1) WHEN (endereco = "000000000001" AND habilita = '1') ELSE
+    entrada(2) WHEN (endereco = "000000000010" AND habilita = '1') ELSE
+    entrada(3) WHEN (endereco = "000000000011" AND habilita = '1') ELSE
+    entrada(4) WHEN (endereco = "000000000100" AND habilita = '1') ELSE
+    entrada(5) WHEN (endereco = "000000000101" AND habilita = '1') ELSE
+    entrada(6) WHEN (endereco = "000000000110" AND habilita = '1') ELSE
+    entrada(7) WHEN (endereco = "000000000111" AND habilita = '1') ELSE
+    entrada(8) WHEN (endereco = "000000001000" AND habilita = '1') ELSE
+    entrada(9) WHEN (endereco = "000000001010" AND habilita = '1') ELSE
+    '0';
 END ARCHITECTURE;
