bd60a9ebc55c2425105e4525ab4c50965a8e78ea
VFermat <vitorge@al.insper.edu.br>
2020-10-17 22:19:50 -0300

    attempt to correct dt

--- vhdl/dividorGenerico_e_Interface.vhd
+++ vhdl/dividorGenerico_e_Interface.vhd
@@ -15,33 +15,29 @@ ENTITY divisorGenerico_e_Interface IS
 END ENTITY;
 
 ARCHITECTURE interface OF divisorGenerico_e_Interface IS
-    SIGNAL sinalUmSegundo   : std_logic_vector(0 DOWNTO 0);
+    SIGNAL sinalUmSegundo   : std_logic;
     SIGNAL saidaclk_reg1seg : std_logic;
     SIGNAL temp             : std_logic_vector(DATA_WIDTH - 1 DOWNTO 0);
 BEGIN
     baseTempo : ENTITY work.divisorGenerico
         GENERIC MAP(
-            divisor => 25000000
+            divisor => 25000
         ) -- divide por 10.
         PORT MAP(
             clk       => clk,
             saida_clk => saidaclk_reg1seg
         );
 
-    registraUmSegundo : ENTITY work.registradorGenerico
-        GENERIC MAP(
-            larguraDados => 1
-        )
+    registraUmSegundo : ENTITY work.flipFlopGenerico
         PORT MAP(
-            DIN    => "1",
+            DIN    => '1',
             DOUT   => sinalUmSegundo,
             ENABLE => '1',
             CLK    => saidaclk_reg1seg,
             RST    => limpaLeitura
         );
 
-    temp(0) <= sinalUmSegundo(0);
     -- Faz o tristate de saida:
-    leituraUmSegundo <= temp WHEN habilitaLeitura = '1' ELSE
+    leituraUmSegundo <= "0000000" & sinalUmSegundo WHEN habilitaLeitura = '1' ELSE
         (OTHERS => 'Z');
 END ARCHITECTURE interface;
